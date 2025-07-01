ae77ab95de3f3f372562fac5a63c735a79720496
spitfire229 <98948641+spitfire229@users.noreply.github.com>
2025-07-01 13:53:08 +0200

    svsv

--- lesupport-app/utils/telecommande/preload.js
+++
@@ -1,5 +0,0 @@
-const { ipcRenderer, contextBridge } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  handleCommand: (cb) => ipcRenderer.on('telecommande-command', cb)
-});
